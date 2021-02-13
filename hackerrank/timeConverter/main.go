0f17e16a7b8bf24e34b1d292df423ecdf6e1c993
Arturo Negreiros <anegreiross@outlook.com>
2021-02-13 15:31:11 -0500

    finished time converter

--- hackerrank/timeConverter/main.go
+++ hackerrank/timeConverter/main.go
@@ -1,63 +1,98 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
-
-	s := "11:59:59PM"
+/*
+ * Complete the timeConversion function below.
+ */
+func timeConversion(s string) string {
 	finalHour := ""
 	splited := strings.Split(s, "")
-
-	//fmt.Println(splited)
-	//fmt.Println(reflect.TypeOf(splited))
-
-	//number, err := strconv.Atoi(splited[1])
-
-	/*if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(12 + number)*/
+	fmt.Println(s)
 
 	for x := 1; x < len(splited)-2; x++ {
 
-		if splited[len(splited)-2] == "P" && splited[1] != "2" {
+		if splited[8]+splited[9] == "PM" {
 
-			finalHour = splited[0] + splited[1] + splited[2] + splited[3] + splited[4] + splited[5] + splited[6] + splited[7]
-			break
+			if splited[0]+splited[1] == "12" {
+				finalHour = splited[0] + splited[1] + splited[2] + splited[3] + splited[4] + splited[5] + splited[6] + splited[7]
+				break
+			} else {
+				if x == 1 {
 
-		} else {
-			if x == 1 {
+					if splited[x-1]+splited[x] != "12" {
 
-				if splited[x-1] == "0" {
+						number, err := strconv.Atoi(splited[x-1] + splited[x])
 
-					number, err := strconv.Atoi(splited[x])
+						if err != nil {
+							fmt.Println(err)
+						}
 
-					if err != nil {
-						fmt.Println(err)
+						finalHour += strconv.Itoa(number + 12)
+
 					}
-
-					finalHour += strconv.Itoa(number + 12)
 
 				} else {
 
-					finalHour += "00"
+					finalHour += splited[x]
+
 				}
-
-			} else {
-
-				finalHour += splited[x]
-
 			}
 
-			//finalHour += splited[x]
+		} else if splited[8]+splited[9] == "AM" {
+
+			if splited[x-1]+splited[x] == "12" {
+				finalHour = "00" + splited[2] + splited[3] + splited[4] + splited[5] + splited[6] + splited[7]
+				break
+			} else {
+				finalHour = splited[0] + splited[1] + splited[2] + splited[3] + splited[4] + splited[5] + splited[6] + splited[7]
+				break
+			}
 		}
+
 	}
 
-	fmt.Println(finalHour)
+	return finalHour
 
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	outputFile, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer outputFile.Close()
+
+	writer := bufio.NewWriterSize(outputFile, 1024*1024)
+
+	s := readLine(reader)
+
+	result := timeConversion(s)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
