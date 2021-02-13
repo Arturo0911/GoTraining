b552f4a14e7ae701d1ea4a99ae8b72a81b98e7c7
Arturo Negreiros <anegreiross@outlook.com>
2021-02-13 13:54:01 -0500

    ok

--- hackerrank/timeConverter/main.go
+++ hackerrank/timeConverter/main.go
@@ -2,17 +2,62 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"strconv"
 	"strings"
 )
 
 func main() {
 
-	name := "07:05:45PM"
-	splited := strings.Split(name, "")
+	s := "11:59:59PM"
+	finalHour := ""
+	splited := strings.Split(s, "")
 
-	fmt.Println(splited)
-	fmt.Println(reflect.TypeOf(splited))
+	//fmt.Println(splited)
+	//fmt.Println(reflect.TypeOf(splited))
 
-	fmt.Println(12 + int(splited[1]))
+	//number, err := strconv.Atoi(splited[1])
+
+	/*if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(12 + number)*/
+
+	for x := 1; x < len(splited)-2; x++ {
+
+		if splited[len(splited)-2] == "A" && splited[0] != "1" {
+
+			finalHour = splited[0] + splited[1] + splited[2] + splited[3] + splited[4] + splited[5] + splited[6] + splited[7]
+			break
+
+		} else {
+			if x == 1 {
+
+				if splited[x-1] == "0" {
+
+					number, err := strconv.Atoi(splited[x])
+
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					finalHour += strconv.Itoa(number + 12)
+
+				} else {
+
+					finalHour += "00"
+				}
+
+			} else {
+
+				finalHour += splited[x]
+
+			}
+
+			//finalHour += splited[x]
+		}
+	}
+
+	fmt.Println(finalHour)
+
 }
