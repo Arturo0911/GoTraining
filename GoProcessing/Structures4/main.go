2e4b9597783164e201db97be79e97b12cf28f1c4
arturo <anegreiross@outlook.com>
2021-04-17 21:22:44 -0500

    Understanding Goroutines

--- GoProcessing/Structures4/main.go
+++ GoProcessing/Structures4/main.go
@@ -3,9 +3,32 @@ package main
 
 import "fmt"
 
+func countChannel(firstChan chan int){
+	x:= 0
+	for {
+		firstChan <- x
+		x++
+	}
+}
+
+func Printing(secondChan chan int){
+	var value int
+	for {
+		value = <- secondChan
+		fmt.Println(value)
+	}
+}
+
+/**
+ * making Goroutines and 
+ * comunication between channels
+ */
 
 func main(){
 
-
-	fmt.Println("Hi!")
+	channel := make(chan int)
+	go countChannel(channel)
+	go Printing(channel)
+	var final string
+	fmt.Scanln(&final)
 }
