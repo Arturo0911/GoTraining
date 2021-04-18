4f22c6c1fb986d2fe728f68bd7daa22c7b731435
arturo <anegreiross@outlook.com>
2021-04-18 00:59:47 -0500

    ok

---
+++ HeadFirstGo/functions/function.go
@@ -0,0 +1,7 @@
+package functions
+
+import "fmt"
+
+func Making() {
+	fmt.Println("Hiiiiiii")
+}
--- HeadFirstGo/main.go
+++ HeadFirstGo/main.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func getStrings(fileName string) ([]string, error) {
+
+	var lines []string
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	err = file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	return lines, nil
+
+}
 
 func main() {
 
-	fmt.Println("Head first Go!!")
+	fileLines, err := getStrings("golang.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(fileLines)
 }
