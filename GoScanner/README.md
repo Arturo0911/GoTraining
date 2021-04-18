81e903c897e33c76c199ac42ad62585a24ca9a58
arturo <anegreiross@outlook.com>
2021-04-17 21:09:05 -0500

    Completed scanner and crated another instances to learn channel useful

---
+++ GoScanner/README.md
@@ -0,0 +1,3 @@
+
+references:
+https://dl.hiva-network.com/Library/security/Black-Hat-Go_Go-Programming-For-Hackers-and-Pentesters.pdf
---
+++ GoScanner/main.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+
+
+The first step in creating the port scanner is understanding how
+to initiate a connection from a client to a server. Throughout
+this example, you’ll be connecting to and scanning
+scanme.nmap.org, a service run by the Nmap project. To do
+this, you’ll use Go’s net package: net.Dial(network, address string).
+The first argument is a string that identifies the kind of
+connection to initiate. This is because Dial isn’t just for TCP; it
+can be used for creating connections that use Unix sockets,
+UDP, and Layer 4 protocols that exist only in your head (the
+authors have been down this road, and suffice it to say, TCP is
+very good). There are a few strings you can provide, but for
+the sake of brevity, you’ll use the string tcp.
+The second argument tells Dial(network, address string) the host
+to which you wish to connect. Notice it’s a single string, not a
+string and an int. For IPv4/TCP connections, this string will take
+the form of host:port. For example, if you wanted to connect to
+scanme.nmap.org on TCP port 80, you would supply
+
+
+*/
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	for i := 1; i < 65535; i++ {
+
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("192.168.100.33:%d", j)
+			connection, err := net.Dial("tcp", address)
+
+			if err != nil {
+				return
+			}
+			connection.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
+
+	}
+
+	wg.Wait()
+}
