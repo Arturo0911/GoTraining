b6cd0db5addb30017b5fdd22d0e5f07eb35632a1
arturo <anegreiross@outlook.com>
2021-04-17 19:19:54 -0500

    ok

--- GoProcessing/Structures3/main.go
+++ GoProcessing/Structures3/main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Behavior interface {
+	Breath() bool // -> is Mamifer or not!
+	Walk() bool
+	Swim() bool
+}
+
+type Animal struct {
+	Name   string
+	Type   string
+	Age    int
+	Height float64
+	Status string
+}
+
+func (p Animal) Breath() bool {
+
+	if p.Type == "Fish" {
+		return false
+	} else {
+		return true
+	}
+
+}
+
+func Load(a *Animal) {
+
+	a.Name = "Arturo"
+	a.Height = 28
+	a.Height = 181
+	a.Status = "Single"
+	a.Type = "Human"
+
+}
 
 func main() {
-
-	fmt.Println("Working")
+	var a Animal
+	Load(&a)
+	fmt.Println(a)
 }
