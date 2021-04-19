c67840811a50907232cfb4571075c56b48809c95
arturo <anegreiross@outlook.com>
2021-04-18 22:25:25 -0500

    ok

--- HeadFirstGo/StructsExcersise/2/SecondExcersise.go
+++ HeadFirstGo/StructsExcersise/2/SecondExcersise.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+type part struct {
+	description string
+	count       int
+}
+
+func doublePack(p *part) {
+
+	p.count *= 2
+}
+
 func main() {
-	fmt.Println("hiii")
+	var fuses part
+
+	fuses.description = "Fuses"
+	fuses.count = 5
+	doublePack(&fuses)
+	fmt.Println(fuses.description)
+	fmt.Println(fuses.count)
 }
