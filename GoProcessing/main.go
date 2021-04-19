20a2c40c900fe144bde1ccc32eafc14693bd4eee
arturo <anegreiross@outlook.com>
2021-04-19 12:44:59 -0500

    ok

--- GoProcessing/main.go
+++ GoProcessing/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"reflect"
 )
 
 // @author Arturo0911
 // Trying to get datasets and use
 // the value like data training
+
+type ModelSet struct {
+	SepalLength float64
+	SepalWidth  float64
+	PetalLength float64
+	PetalWidth  float64
+	Species     string
+}
 
 func LoadDataSets(fileName string) ([]string, error) {
 	//  in this case we gonna build a dataset with the file name or
@@ -40,9 +50,39 @@ func LoadDataSets(fileName string) ([]string, error) {
 	}
 
 	return lines, err
+}
+
+/**
+* @return slices with the data inside
+ */
+func ReaderFile(fileName string) [][]string {
+
+	recordFile, err := os.Open(fileName)
+
+	if err != nil {
+
+		fmt.Println(err)
+	}
+
+	reader := csv.NewReader(recordFile)
+
+	records, _ := reader.ReadAll()
+
+	//fmt.Println(records)
+	fmt.Println(reflect.ValueOf(records).Kind())
+	/*for _, value := range records {
+
+		if value[4] == "Iris-setosa" {
+			fmt.Println(value)
+			break
+		}
+
+	}*/
+
+	return records
 
 }
 
 func main() {
-	fmt.Println("hi everyone")
+	ReaderFile("datasets/iris.csv")
 }
