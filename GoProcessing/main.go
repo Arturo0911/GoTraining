787c2fb0cb8d79d965d38070e1e4884d81bee0fb
arturo <anegreiross@outlook.com>
2021-04-19 13:35:15 -0500

    ok

--- GoProcessing/main.go
+++ GoProcessing/main.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
+	"math"
 	"os"
-	"reflect"
 )
 
 // @author Arturo0911
@@ -30,59 +30,77 @@ func LoadDataSets(fileName string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	scanner := bufio.NewScanner(file)
-
 	for scanner.Scan() {
 
 		line := scanner.Text()
 		lines = append(lines, line)
 	}
-
 	err = file.Close()
 
 	if err != nil {
 		return nil, err
 	}
-
 	if scanner.Err() != nil {
 		return nil, scanner.Err()
 	}
-
 	return lines, err
+}
+
+/**
+* @description Math process
+* 				Making a Average, medium, moda, Variance
+*
+*
+*		s²x = var(x)= 	∑n i=1	(xi−x^)²
+*						---------------
+*							n−1
+*
+ */
+
+// Variance, standart desviation, average
+func Variance(parameterList []float64, y []float64) (float64, float64, float64) {
+
+	var initSum float64 = 0
+	var finalSum float64 = 0
+
+	var newVariance float64 = 0
+	var standartDesviation float64 = 0
+	var average float64 = 0
+
+	for _, value := range parameterList {
+		initSum += value
+	}
+
+	average = (initSum / float64(len(parameterList)))
+
+	for i := 0; i < len(parameterList); i++ {
+		finalSum += math.Pow((parameterList[i] - average), 2)
+	}
+
+	newVariance = (finalSum / float64((len(parameterList) - 1)))
+	standartDesviation = math.Pow(newVariance, 0.5)
+
+	return average, newVariance, standartDesviation
+
 }
 
 /**
 * @return slices with the data inside
  */
 func ReaderFile(fileName string) [][]string {
-
 	recordFile, err := os.Open(fileName)
 
 	if err != nil {
-
 		fmt.Println(err)
 	}
-
 	reader := csv.NewReader(recordFile)
-
 	records, _ := reader.ReadAll()
-
-	//fmt.Println(records)
-	fmt.Println(reflect.ValueOf(records).Kind())
-	/*for _, value := range records {
-
-		if value[4] == "Iris-setosa" {
-			fmt.Println(value)
-			break
-		}
-
-	}*/
 
 	return records
 
 }
 
 func main() {
-	ReaderFile("datasets/iris.csv")
+	fmt.Println(ReaderFile("datasets/iris.csv"))
 }
