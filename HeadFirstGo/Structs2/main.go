8dc42b58eeaf5b99e4ebeb790c73c6378a395582
arturo <anegreiross@outlook.com>
2021-04-18 21:41:22 -0500

    using pointers in the structs

--- HeadFirstGo/Structs2/main.go
+++ HeadFirstGo/Structs2/main.go
@@ -4,8 +4,27 @@ package main
 import "fmt"
 
 
+type suscriber struct {
+
+	name 	string
+	rate 	float64
+	active 	bool
+}
+
+func applyDiscount(s *suscriber){
+
+	s.rate = 4.99
+}
+
+
+
 func main(){
 
 
 	fmt.Println("hi!!")
+	var s suscriber
+
+	applyDiscount(&s)
+
+	fmt.Println(s.rate)
 }
