df72d10b25bcaeb68bd7801fcc16540a546d2ccb
arturo <anegreiross@outlook.com>
2021-04-22 02:26:21 -0500

    ok

--- GoRoutines/ChannelComunication2/main.go
+++ GoRoutines/ChannelComunication2/main.go
@@ -12,6 +12,7 @@ import (
 // source of information
 // programacion Go ya
 // https://www.tutorialesprogramacionya.com/goya/
+// https://www.udemy.com/course/lenguaje-go
 
 
 func myNameSlow(name string){
@@ -28,10 +29,31 @@ func myNameSlow(name string){
 }
 
 
+func loop(channel1 chan time.Duration){
+	init := time.now()
 
+	for i:= 0; i <  1000000000000; i++{
+
+	}
+
+	final := time.Now()
+
+
+	channel1 <- final.Sub(init)
+	 
+
+}
 
 
 func main() {
 	
 	myNameSlow("Arturo")
+
+	channel1 := make(chan time.Duration)
+
+	go loop(channel1)
+	fmt.Println("i'm here")
+
+
+
 }
