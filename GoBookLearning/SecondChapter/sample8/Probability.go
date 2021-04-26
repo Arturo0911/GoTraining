a945c8d84183edce7962c0bb7bf934b648165639
arturo <anegreiross@outlook.com>
2021-04-26 18:21:29 -0500

    ok

---
+++ GoBookLearning/SecondChapter/sample8/Probability.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"gonum.org/v1/gonum/stat"
+)
+
+func Probability() {
+
+	observed := []float64{48, 52}
+	expected := []float64{50, 50}
+
+	chiSquare := stat.ChiSquare(observed, expected)
+	fmt.Printf("Chi square value is %0.2f\n\n", chiSquare)
+}
+
+func main() {
+	Probability()
+}
