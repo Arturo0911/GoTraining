29575478a106415cb300a0261684d011b317c347
arturo <anegreiross@outlook.com>
2021-04-27 15:42:35 -0500

    Breka

--- GoBookLearning/ThirdChapter/sample3/Confusion.go
+++ GoBookLearning/ThirdChapter/sample3/Confusion.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"gonum.org/v1/gonum/integrate"
+	"gonum.org/v1/gonum/stat"
+)
+
 /*
 Confusion matrices, AUC, and ROC
 
@@ -17,12 +24,32 @@ and columns corresponding to categories that were predicted.
 Then, the value of each element is the corresponding count:
 
 								Predicted
+
 						Fraud		Not fraud
 Observed	fraud		  TP			FN
 			Not fraund	  FP			TN
 
 */
 
-func main() {
+func CalculateAUCROC() {
 
+	// Define our scores and classes.
+	scores := []float64{0.1, 0.35, 0.4, 0.8}
+	classes := []bool{true, false, true, false}
+
+	// Calculate the true positive rates (recalls) and
+	// false positive rates.
+	tpr, fpr, _ := stat.ROC(nil, scores, classes, nil)
+
+	// Compute the Area Under Curve.
+	auc := integrate.Trapezoidal(fpr, tpr)
+
+	// Output the results to standard out.
+	fmt.Printf("true  positive rate: %v\n", tpr)
+	fmt.Printf("false positive rate: %v\n", fpr)
+	fmt.Printf("auc: %v\n", auc)
+}
+
+func main() {
+	CalculateAUCROC()
 }
