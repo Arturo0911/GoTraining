20f5062b1a0e2c0ffdbc825b79679d3533abcad4
arturo <anegreiross@outlook.com>
2021-04-27 23:51:21 -0500

    Still for review

--- Hackerrank/StatisticsDays/Quartiles/Quartil.go
+++ Hackerrank/StatisticsDays/Quartiles/Quartil.go
@@ -14,28 +14,23 @@ func quartiles(arr []int32) []int32 {
 	var mediamPos int
 	var value int32 = 0
 
-	for i := len(arr); i >= 0; i-- {
+	for x := len(arr) - 1; x >= 0; x-- {
+		for y := len(arr) - 1; y > len(arr)-1-x; y-- {
 
-		for j := 0; j > len(arr)-i; j-- {
-
-			if arr[j] > arr[j-1] {
-
-				arr[j-1], arr[j] = arr[j], arr[j-1]
-
+			if arr[y] < arr[y-1] {
+				//fmt.Println(arr[y], arr[y-1])
+				arr[y], arr[y-1] = arr[y-1], arr[y]
 			}
-
 		}
 	}
-
-	fmt.Println(arr)
 
 	if len(arr)%2 != 0 {
 
 		// position number normal
 		mediamPos = (len(arr) / 2) + 1
 		value = arr[mediamPos-1]
-		lowQuartile = (arr[(mediamPos/2)-1] + arr[(mediamPos/2)]) / 2
-		highQuartile = (arr[(mediamPos+(len(arr)-mediamPos)/2)-1] + arr[(mediamPos+(len(arr)-mediamPos)/2)]) / 2
+		lowQuartile = (arr[((len(arr)-mediamPos)/2)] + arr[((len(arr)-mediamPos)/2)-1]) / 2
+		highQuartile = (arr[(mediamPos+((len(arr)-mediamPos)/2))-1] + arr[(mediamPos+((len(arr)-mediamPos)/2))]) / 2
 
 		quartile = append(quartile, lowQuartile)
 		quartile = append(quartile, value)
@@ -43,9 +38,10 @@ func quartiles(arr []int32) []int32 {
 		return quartile
 
 	} else {
-		mediamPos = (len(arr) / 2)
-		mediamPos2 := (len(arr) / 2) + 1
-		lowQuartile = (arr[(mediamPos/2)-1] + arr[(mediamPos/2)]) / 2
+		mediamPos = (len(arr) / 2) - 1
+		mediamPos2 := (len(arr) / 2)
+		value = (arr[mediamPos] + arr[mediamPos2]) / 2
+		lowQuartile = (arr[((len(arr)-mediamPos)/2)] + arr[((len(arr)-mediamPos)/2)-1]) / 2
 		highQuartile = (arr[(mediamPos+(len(arr)-mediamPos2)/2)-1] + arr[(mediamPos2+(len(arr)-mediamPos)/2)]) / 2
 
 		quartile = append(quartile, lowQuartile)
@@ -59,8 +55,9 @@ func quartiles(arr []int32) []int32 {
 
 func main() {
 
-	arr := []int32{3, 7, 8, 5, 12, 14, 21, 13, 18}
-	//arr2 := []int32{3, 7, 8, 5, 12, 14, 21, 13, 18, 20}
+	//arr := []int32{3, 7, 8, 5, 12, 14, 21, 13, 18}
+	arr := []int32{3, 7, 8, 5, 12, 14, 21, 15, 18, 14}
+	fmt.Println(arr)
 	fmt.Println(quartiles(arr))
 	//fmt.Println(quartiles(arr2))
 }
