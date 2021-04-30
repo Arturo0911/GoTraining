75d3752453c7cc7d9223a877d250b700ced0f271
arturo <anegreiross@outlook.com>
2021-04-30 02:22:36 -0500

    everithing ok

--- GoBookLearning/FourthChapter/Regression/Regression.go
+++ GoBookLearning/FourthChapter/Regression/Regression.go
@@ -1,46 +1,136 @@
 package main
 
-/*
+import (
+	"fmt"
+	"log"
+	"os"
 
-Differents types associated to regression:
+	"github.com/go-gota/gota/dataframe"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+)
 
-	Response or dependent variable:
-		These terms will be used interchangeably for the variable
-		tahta we are trying to predict based on one or more other
-		variables. This variable is iften labeld y
+func CheckingCorrelation(pathFile string) {
 
+	file, err := os.Open(pathFile)
 
-	Explanatory variables, independent variables, features attributes,
-	or regressors:
-		These terms will be used interchangeably for the
-		variables that we aore using to predict the response. These variabls
-		are often labeled x or x 1 , x 2 , and so on
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	dataCSV := dataframe.ReadCSV(file)
 
-	Linear regression:
-		This type of refressions assumes that the dependent
-		variable depends on the independent variable, linearly
-		(that is, following the equation for a line).
+	// Extract the target of the column
+	// chosse the column, and the name
+	yVals := dataCSV.Col("Sales").Float()
 
+	// create a scatter a polot for each of the features in the
+	// dataset
 
-	Nonlinear regression:
-		This type of regression assumes that the dependent
-		variable dependes on th independent variable in a relashionship
-		that is no linear(for example, polynomial or exponential)
+	for _, colName := range dataCSV.Names() {
+		// pts will hold the value for plotting
 
+		pts := make(plotter.XYs, dataCSV.Nrow())
 
-	Multiple regression:
-		A regression with more that one independent variable.
+		// fill pts with data.
 
-	Fitting or training:
-		The process of parameterizing a model, such as regresion model,
-		so that it can predict a certgain dependent variable.
+		for i, floatVal := range dataCSV.Col(colName).Float() {
+			pts[i].X = floatVal
+			pts[i].Y = yVals[i]
+		}
 
+		// create the plot
 
-	Prediction:
-		The process of using a parameterized model, such as regression mdoel,
-		to predict a certain dependent variable.
+		p := plot.New()
 
+		p.X.Label.Text = colName
+		p.Y.Label.Text = "y"
 
-*/
+		p.Add(plotter.NewGrid())
 
-func main() {}
+		s, err := plotter.NewScatter(pts)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		s.GlyphStyle.Radius = vg.Points(3)
+
+		// save the plot to a PNG file
+
+		if err := p.Save(4*vg.Inch, 4*vg.Inch, colName+"_scatter.png"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
+func ReadingAdvertising(pathFile string) {
+
+	file, err := os.Open(pathFile)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	advertDF := dataframe.ReadCSV(file)
+
+	/*// Use the describe metjod to calculate
+	// summary statistics for all the columns in one shot
+
+	adverSummary := advertDF.Describe()
+
+	fmt.Println(adverSummary)*/
+
+	// Extract the target column.
+	//yVals := advertDF.Col("Sales").Float()
+
+	// create a histogram for each of the columns in the dataset.
+	for _, colName := range advertDF.Names() { // Printing the headers
+		// create a plotter. values value and fill it with the
+		// values from the respective column of the dataframe.
+
+		plotVals := make(plotter.Values, advertDF.Nrow())
+
+		for i, floatVal := range advertDF.Col(colName).Float() {
+			plotVals[i] = floatVal
+		}
+
+		// Making a plot and set its title
+
+		p := plot.New()
+		p.Title.Text = fmt.Sprintf("Histogram of a %s", colName)
+
+		// Create a histogram of our values drawn
+		// from the standard normal.
+
+		h, err := plotter.NewHist(plotVals, 16)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Normalizing the data.
+		h.Normalize(1)
+
+		// Add the histogram to the plot.
+
+		p.Add(h)
+
+		// saving the plot to a PNG file.
+
+		if err := p.Save(4*vg.Inch, 4*vg.Inch, colName+"_hist.png"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
+func main() {
+	pathFile := "../Advertising.csv"
+	//readingAdvertising(pathFile)
+	CheckingCorrelation(pathFile)
+}
