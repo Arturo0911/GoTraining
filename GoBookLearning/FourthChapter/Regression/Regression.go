b6d0b315473679cd6e8f3f4ce87e38a48b06810d
arturo <anegreiross@outlook.com>
2021-04-30 00:08:53 -0500

    ok

---
+++ GoBookLearning/FourthChapter/Regression/Regression.go
@@ -0,0 +1,46 @@
+package main
+
+/*
+
+Differents types associated to regression:
+
+	Response or dependent variable:
+		These terms will be used interchangeably for the variable
+		tahta we are trying to predict based on one or more other
+		variables. This variable is iften labeld y
+
+
+	Explanatory variables, independent variables, features attributes,
+	or regressors:
+		These terms will be used interchangeably for the
+		variables that we aore using to predict the response. These variabls
+		are often labeled x or x 1 , x 2 , and so on
+
+	Linear regression:
+		This type of refressions assumes that the dependent
+		variable depends on the independent variable, linearly
+		(that is, following the equation for a line).
+
+
+	Nonlinear regression:
+		This type of regression assumes that the dependent
+		variable dependes on th independent variable in a relashionship
+		that is no linear(for example, polynomial or exponential)
+
+
+	Multiple regression:
+		A regression with more that one independent variable.
+
+	Fitting or training:
+		The process of parameterizing a model, such as regresion model,
+		so that it can predict a certgain dependent variable.
+
+
+	Prediction:
+		The process of using a parameterized model, such as regression mdoel,
+		to predict a certain dependent variable.
+
+
+*/
+
+func main() {}
