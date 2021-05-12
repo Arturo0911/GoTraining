b4f8fe2d1032844d68e36f70e727f26637eebe39
arturo <anegreiross@outlook.com>
2021-05-12 16:13:17 -0500

    recover chapter

---
+++ Simulation/BinomialDistribution/binomial_distribution.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Arturo0911/GoTraining/Simulation/BinomialDistribution/evaluation"
+)
+
+func CLI() {
+	fmt.Println("USE THE FOLLOWING COMMANDS TO GET THE RESULT")
+}
+
+func main() {
+	fmt.Println(evaluation.Factorial(4))
+}
