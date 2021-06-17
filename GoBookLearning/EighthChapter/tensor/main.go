c23e94df2df1ad837e5a1092375a1dfbe2422fa6
Arturo Negreiros <anegreiross@outlook.com>
2021-06-17 14:59:44 -0500

    change modle

--- GoBookLearning/EighthChapter/tensor/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-
-	// Load the serialized graphDef from a file.
-
-	modelfile, labelsfile, err := modelFiles(*modeldir)
-
-}
--- GoBookLearning/SecondChapter/README.md
+++
@@ -1,2 +0,0 @@
-Matrix, probability and statistics
-=================================
