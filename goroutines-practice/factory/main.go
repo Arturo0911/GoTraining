a89a43f1b2ae402fb666079cea800002e166b3d5
Arturo Negreiros <anegreiross@outlook.com>
2021-08-30 13:02:09 -0500

    updated inheritance and interfaces

---
+++ goroutines-practice/factory/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// only for notifications sms and email
+
+type INotificationFactory interface {
+	SendNotification()
+	GetSender() ISender
+}
+
+type ISender interface {
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+type SMSNotification struct {
+}
+
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending notification via sms")
+}
