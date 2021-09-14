8e7b8900cee5f18bab93f13a66d6fcf8c496512a
Payload <anegreiross@outlook.com>
2021-09-14 17:01:21 -0500

    ok

--- GoRoutines/ChannelComunication2/main.go
+++ GoRoutines/ChannelComunication2/main.go
@@ -1,59 +1,48 @@
 package main
 
 import (
-
 	"fmt"
 	"strings"
 	"time"
 )
-
-
 
 // source of information
 // programacion Go ya
 // https://www.tutorialesprogramacionya.com/goya/
 // https://www.udemy.com/course/lenguaje-go
 
+func myNameSlow(name string) {
 
-func myNameSlow(name string){
-	
 	letras := strings.Split(name, "")
 	//fmt.Println(letras)
 
 	for _, letra := range letras {
-		time.Sleep(1 *time.Second)
+		time.Sleep(1 * time.Second)
 		fmt.Println(letra)
 	}
 
-
 }
 
+// func loop(channel1 chan time.Duration) {
+// 	init := time.now()
 
-func loop(channel1 chan time.Duration){
-	init := time.now()
+// 	for i := 0; i < 1000000000000; i++ {
 
-	for i:= 0; i <  1000000000000; i++{
+// 	}
 
-	}
+// 	final := time.Now()
 
-	final := time.Now()
+// 	channel1 <- final.Sub(init)
 
-
-	channel1 <- final.Sub(init)
-	 
-
-}
-
+// }
 
 func main() {
-	
+
 	myNameSlow("Arturo")
 
 	channel1 := make(chan time.Duration)
 
 	go loop(channel1)
 	fmt.Println("i'm here")
-
-
 
 }
